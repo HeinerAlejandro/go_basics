7f04326506e9bc3f4d5a889311bfbd6291255618
HeinerAlejandro <heiner.enis@gmail.com>
2024-01-16 10:09:51 -0300

    Add float conversions

--- datatypes-conv/datatypesconv.go
+++ datatypes-conv/datatypesconv.go
@@ -6,6 +6,9 @@ func main() {
 	var myInt32 int32 = 56
 	var myInt16 int16 = 12
 
-	fmt.Println(int32(myInt16) + myInt32)
+	var myFloat32 float32 = 49.9
+
+	fmt.Println("Suma de diferentes tipos de enteros", int32(myInt16)+myInt32)
+	fmt.Println("Suma de diferentes de entero y floatante", float32(myInt16)+myFloat32)
 
 }
