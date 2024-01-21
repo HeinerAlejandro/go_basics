92a72203a78cb16345b15fa33696a183ffb1c05b
HeinerAlejandro <heiner.enis@gmail.com>
2024-01-21 10:37:06 -0300

    Add constant usage

--- operators/operators.go
+++ operators/operators.go
@@ -13,4 +13,12 @@ func main() {
 	fmt.Printf("Este es PI: %f.\n", math.Pi)
 	fmt.Printf("Este es Euler: %f.\n", math.E)
 	fmt.Printf("Otra constante: %f.\n", math.Log10E)
+
+	fmt.Println("Algunas constantes mas:")
+	fmt.Printf(
+		"|%5s     |     %.4f     |\n|%5s     |     %.4f     |\n|%5s     |     %.4f     |\n",
+		"Phi", math.Phi,
+		"SQRt2", math.Sqrt2,
+		"SQRtPhi", math.SqrtPhi,
+	)
 }
